d01194affc9383d9eeb9d12bd76bd08c194e9ef8
Hack Reactor Student <student@hackreactor.com>
2017-09-22 22:04:27 -0700

    Update dev-dependencies for style/css loaders. Add header/nav bar to App component. TODO: Fix webpack so it can load css from @blueprintjs.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import ChatBox from './chatBox.jsx'
-import Canvas from './canvas.jsx'
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header.jsx';
+import ChatBox from './chatBox.jsx';
+import Canvas from './canvas.jsx';
 //import openSocket from 'socket.io-client';
 
-// asdfjlsadf;fasafafsd;afds
-//fskl;fdafdsa;afsdafsd;asdfkl;afsd
 export default class App extends React.Component{
-	constructor(){
+	constructor() {
 		super()
 	}
 
-	render(){
-		return(
+	render() {
+		return (
 			<div>
+				<BrowserRouter>
+					<Header />
+				</BrowserRouter>
 				<ChatBox />
 				<Canvas />
-			</div>
-		
-		)
+			</div>		
+		);
 	}
-
 }
