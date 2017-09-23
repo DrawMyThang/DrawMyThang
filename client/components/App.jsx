757f015bd900bf28f1cbd64b4107674240065e80
Jordan Daniels <decentpianist@gmail.com>
2017-09-22 18:09:17 -0700

    test pushing into brances

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,8 @@ import ChatBox from './chatBox.jsx'
 import Canvas from './canvas.jsx'
 //import openSocket from 'socket.io-client';
 
-
+// asdfjlsadf;fasafafsd;afds
+//fskl;fdafdsa;afsdafsd;asdfkl;afsd
 export default class App extends React.Component{
 	constructor(){
 		super()
