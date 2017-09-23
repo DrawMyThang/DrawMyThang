538c8fa48160c263b9fe5df382d37d93aa1241c3
Hack Reactor Student <student@hackreactor.com>
2017-09-23 14:02:25 -0700

    canvas drawing logic

--- client/scripts/canvasLogic.js
+++ client/scripts/canvasLogic.js
@@ -2,11 +2,27 @@ $(document).ready(function(){
     var canvas = document.getElementById("draw-comp");
     console.log(document.getElementById("draw-comp"));
     var context = canvas.getContext('2d');
+    var enableDraw = false;
     context.fillStyle = "#FF0000";
-    var draw = function () {
+    var draw = function (mouseX, mouseY) {
         context.beginPath();
         context.fillRect(20, 20, 150, 100);
     };
 
+  
+    var test = function (event) {
+        if (enableDraw) {
+            console.log(event.clientX);
+            console.log(event.clientY);
+        }
+    }   
+    $('#draw-comp').mousemove(test);
+
+    $('#draw-comp').mousedown(() => {
+        enableDraw = true;
+    });
+    $('#draw-comp').mouseup(() => {
+        enableDraw = false
+    });
     draw();
 });
