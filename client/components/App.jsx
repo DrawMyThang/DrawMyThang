ba0eb9c21a422b6c9f219fab83eab37733748c06
Jordan Daniels <decentpianist@gmail.com>
2017-09-24 19:18:38 -0700

    user id displays in user box component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,12 +3,10 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header.jsx';
 import ChatBox from './chatBox.jsx';
 import Canvas from './canvas.jsx';
-//import openSocket from 'socket.io-client';
+import UserBox from './userBox.jsx';
 
 class App extends React.Component {
-  // constructor() {
-  //   super();
-  // }
+ 
   render() {
     return (
       <div>
@@ -17,7 +15,7 @@ class App extends React.Component {
         </BrowserRouter>
       <div id ="whole">
         <section className="sidebar">
-          <div className = "users"> this will eventually be users box</div>
+          <UserBox />
           <ChatBox />
         </section>
         <Canvas />
