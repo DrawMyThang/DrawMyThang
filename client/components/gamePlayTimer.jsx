52b92941a6856950e08d074159cb126d617baac1
Jordan Daniels <decentpianist@gmail.com>
2017-09-26 11:57:48 -0700

    game play timer starting to work need to make timer start at 60

---
+++ client/components/gamePlayTimer.jsx
@@ -0,0 +1,37 @@
+import App from './App.jsx'
+import React from 'react'
+import socket from 'socket.io-client';
+
+export default class GamePlayTimer extends React.Component{
+	constructor(){
+		super()
+		this.state = {
+			time: "not started",
+		}
+		this.handleTimer = this.handleTimer.bind(this);
+		this.connectSocket = this.connectSocket.bind(this);
+	}
+
+	connectSocket(){
+		this.socket = socket('http://localhost:8080');
+    this.socket.on('timer', this.handleTimer);
+	}
+
+	handleTimer(time){
+		this.setState({
+			time: time
+		})
+	}
+
+	render(){
+		return(
+
+			<div>
+				<div id="gamePlayTimer" >Game Play Timer: {this.state.time} </div>
+				<button id="gamePlayTimerButton" onClick={this.connectSocket}> Click to Start Timer </button>
+			</div>
+
+			)
+	}
+
+}
--- server/server.js
+++ server/server.js
@@ -20,6 +20,13 @@ const io = socket(server);
 
 const userArr = [];
 
+let countdown = 100; 
+
+setInterval(() => {  
+  countdown--;
+  console.log(countdown, "countdown")
+  io.emit('timer', countdown);
+}, 1000);
 
 io.on('connection', (socks) => {
   console.log('user is connected id:', socks.id);
@@ -28,26 +35,33 @@ io.on('connection', (socks) => {
 		io.emit('drawing', drawData);
 	});
 
+
   socks.on('chat message', (msg) => {
     console.log('message: ', msg, 'id: ', socks.id);
     io.emit('chat message', msg);
-  	});
+  });
 
+  socks.on('user id', () => {
+    userArr.push(socks.id)
+    console.log(userArr, 'userArr')
+    io.emit('user id', userArr)
+    console.log("user id emitting", userArr)
+  });
 
-    socks.on('user id', () => {
-    	userArr.push(socks.id)
-    	console.log(userArr, 'userArr')
-  	io.emit('user id', userArr)
-  	console.log("user id emitting", userArr)
-  	})
+  socks.on('reset', function (data) {
+    countdown = 60;
+    io.sockets.emit('timer', countdown);
+  });
  
 
   socks.on('disconnect', () => {
-  	if (userArr.includes(socks.id)){
-  		userArr.splice(userArr.indexOf(socks.id), 1)
-  		io.emit('disconnect', userArr)
-  	}
-  	console.log(userArr, "userArr after disconnect")
+    if (userArr.includes(socks.id)){
+      userArr.splice(userArr.indexOf(socks.id), 1)
+      io.emit('disconnect', userArr)
+    }
+    console.log(userArr, "userArr after disconnect")
     console.log('user ', socks.id, ' disconnected');
   });
+
+
 });
