fab25918aafa44fa3df51e9f1388d99330bb128c
Jordan Daniels <decentpianist@gmail.com>
2017-09-30 13:55:03 -0700

    artist color defaults to black

--- client/components/canvas.jsx
+++ client/components/canvas.jsx
@@ -50,7 +50,7 @@ class Canvas extends React.Component {
   isArtist(uid) {
     if (uid === this.props.uid) {
       this.state.isArtist = true;
-      this.state.color = 'black';
+      this.currentColor.color = 'black';
     } else {
       this.state.isArtist = false;
     }
